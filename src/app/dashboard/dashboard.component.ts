89f90bf0d2fb6ec50f90cdb812f93563199206c0
Rohit Pathare <124705801+rp4491@users.noreply.github.com>
2023-09-24 15:42:06 +0530

    --commit for dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api-service.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +9,16 @@ import { ApiService } from '../services/api-service.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService, private router: Router, private toasterService: ToastrService) {
     this.getContacts()
   }
   async getContacts() {
     let contacts = await this.apiService.getContacts();
     console.log(contacts)
+  }
+  logOut() {
+    sessionStorage.removeItem('userToken');
+    this.toasterService.success('Log Out Successfully !', 'Log Out')
+    this.router.navigate(['/admin'])
   }
 }
